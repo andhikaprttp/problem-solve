095d0d296917a203764f4f328e6c18bad9984c3a
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-04-09 15:39:00 +0700

    Create flight-time.py

---
+++ python/flight-time.py
@@ -0,0 +1,23 @@
+# Question
+# You are in the airport and you need calculate
+# the flight time of upcoming trip.
+# You are flying from LA to Sydney, covering a distance
+# of 7425 miles, and plane flies at average
+# speed of 550 miles and hour.
+
+# Task
+# Calculate and output the total flight time in hours
+# The result should be a float
+
+# Answer
+
+d = 7425;
+a = 550;
+
+c =  float(d) / float(a);
+
+print(c)
+
+# d for distance and a for average speed of plane.
+# c for d divided a ,
+# and output is float = 13.5
